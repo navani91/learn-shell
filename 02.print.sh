66132ea6a0ae015e7bdbe5d7b979b32dc45ef84c
navaneetha <navani91@gmail.com>
2023-05-07 16:30:02 +0100

    Print in Shell

--- 02.print.sh
+++ 02.print.sh
@@ -1,1 +1,13 @@
 echo Hello World
+
+## color syntax
+# echo -e "\e [COLm MESSAGE \e[0m"
+# echo -e ------enable colors from echo command
+# \e[COLm------Enable a particular color
+# COLOR             CODES
+# RED                 31
+# GREEN               32
+# YELLOW              33
+# BLUE                34
+# MAGENTA             35
+# CYAN                36
