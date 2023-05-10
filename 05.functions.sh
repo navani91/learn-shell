5fb3a3e34939d88d25c9d29ab82058701cba90b4
navaneetha <navani91@gmail.com>
2023-05-10 16:09:00 +0100

    Roboshop-Shell

---
+++ 05.functions.sh
@@ -0,0 +1,14 @@
+## declare a function
+
+xyz() {
+  echo Hello from Function
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments  - $*
+  echo No of Arguments - $#
+}
+
+
+## main programm
+## call a function
+xyz 123
