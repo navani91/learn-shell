86622555f9b55f45fbcfa2912e1258d551bcd715
navaneetha <navani91@gmail.com>
2023-05-10 13:15:08 +0100

    Print in Shell

--- 04.inputs.sh
+++ 04.inputs.sh
@@ -1,3 +1,11 @@
-echo $1
-echo $2
+# 0-n, $*/$@, $#
+
+echo First argument $1 # First argument
+echo Second Argument $2 # Second Argument
+
+echo script Name $0 # script name
+echo All Arguments = $*
+echo All Arguments = $@ # All Arguments
+echo number of Arguments = $# # Number of Arguments
+
 
