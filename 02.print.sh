9a6baaea435ebef0dd3d765fdeda30c10ea4daf6
navaneetha <navani91@gmail.com>
2023-05-10 12:31:56 +0100

    Print in Shell

--- 02.print.sh
+++ 02.print.sh
@@ -14,5 +14,5 @@ echo Hello World
 
 # /e[0m -> zero code is going to reset the color, meaning if we enable color for sure we need to disable it, otherwise the color will continue on screen.
 # we can we double or single quotes. However double quotes are preferred.
-# echo -e "/e[31mHello in Red Color /e[0m"
+# echo -e "\e[31mHello in Red Color \e[0m"
 echo Hello World
