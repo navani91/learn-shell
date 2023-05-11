102b2e35c011d5dff78cd44409109e95a6f59295
navaneetha <navani91@gmail.com>
2023-05-11 14:40:05 +0100

    Roboshop-Shell

---
+++ 06.conditions.sh
@@ -0,0 +1,12 @@
+number=$1
+string=$2
+
+if [ "${number} eq 5"]; then # then can go in next line as well, also semi colon is also fine in same line
+   echo number is 5
+fi
+
+if [ "${string}" abc]; then
+  echo string is abc
+  fi
+
+ ## it is also a good practice to quote the variables in expressions
