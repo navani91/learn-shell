ccfafbfb9622225d900392318a0ad6f3ba4c6823
navaneetha <navani91@gmail.com>
2023-05-11 14:42:58 +0100

    Roboshop-Shell

--- 06.conditions.sh
+++ 06.conditions.sh
@@ -1,11 +1,11 @@
 number=$1
 string=$2
 
-if [ "${number} eq 5"]; then # then can go in next line as well, also semi colon is also fine in same line
+if [ "${number}" eq 5]; then # then can go in next line as well, also semi colon is also fine in same line
    echo number is 5
 fi
 
-if [ "${string}" abc]; then
+if [ "${string}" ==abc]; then
   echo string is abc
   fi
 
